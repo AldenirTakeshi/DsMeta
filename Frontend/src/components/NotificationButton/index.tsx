38b4eb6e78e749e9347fbd6651a0b22b8217fe10
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2022-11-19 08:43:34 -0400

    Send notification

--- Frontend/src/components/NotificationButton/index.tsx
+++ Frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,22 @@
+import axios from "axios";
 import React from "react";
 import icon from "../../assets/Notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
 import "./styles.css";
 
-const NotificationButton = () => {
+type Props = {
+  saleId: number;
+};
+
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+    console.log("Sucesso");
+  });
+}
+
+const NotificationButton = ({ saleId }: Props) => {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notificar" />
     </div>
   );
